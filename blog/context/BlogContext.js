3c4b79a0e554480e18b050986bb6e51f7c31d241
Eda Dural <edaadurall@gmail.com>
2023-12-20 17:11:58 +0300

    useReducer ekleme

--- blog/context/BlogContext.js
+++ blog/context/BlogContext.js
@@ -1,21 +1,37 @@
-import React, { useState } from "react";
+import React, { useReducer, useState } from "react";
 
 const BlogContext = React.createContext();
 
+const blogReducer = (state, action) => {
+    switch (action.type) {
+        case 'add_blogpost':
+            return [...state, { title: 'Vue Js' }];
+        default:
+            return state;
+    }
+};
+
 export const BlogProvider = ({ children }) => {
 
-    const [blogPost, setBlogPost] = useState([
+    // const [blogPosts, setBlogPosts] = useState([
+    //     { title: 'React Native' },
+    //     { title: 'Javascript' },
+    // ]);
+
+    const [blogPosts, dispatch] = useReducer(blogReducer, [
         { title: 'React Native' },
         { title: 'Javascript' },
     ]);
 
     const addBlogPost = () => {
-        setBlogPost([...blogPost, { title: 'Vue Js' }]);
+        // setBlogPost([...blogPost, { title: 'Vue Js' }]);
+
+        dispatch({ type: 'add_blogpost' })
     }
 
     return (
         <BlogContext.Provider
-            value={{ data: blogPost, addBlogPost }}
+            value={{ data: blogPosts, addBlogPost }}
         >
             {children}
         </BlogContext.Provider>
