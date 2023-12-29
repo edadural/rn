3c7d778ab6461c4d85edf732d85930cb5a506d82
Eda Dural <edaadurall@gmail.com>
2023-12-29 17:11:32 +0300

    css

--- 1-oyun/screens/GameScreen.js
+++ 1-oyun/screens/GameScreen.js
@@ -98,6 +98,7 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         padding: 30,
+        paddingTop: 70,
     },
     buttonsContainer: {
         flexDirection: 'row',
