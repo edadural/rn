142bad9714c3f1021f3c51ceef5b76955da1affc
Eda Dural <edaadurall@gmail.com>
2023-12-30 09:33:42 +0300

    auth

---
+++ 2-auth/App.js
@@ -0,0 +1,28 @@
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from './screens/LoginScreen';
+import SignupScreen from './screens/SignupScreen';
+
+const Stack = createNativeStackNavigator();
+
+function NormalStack() {
+  return (
+    <Stack.Navigator >
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Signup" component={SignupScreen} />
+    </Stack.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <NormalStack />
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+
+});
