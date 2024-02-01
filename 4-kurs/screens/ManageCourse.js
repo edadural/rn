20a9667acd6dd71bf532e3a1f0cadd5898bf4484
Eda Dural <edaadurall@gmail.com>
2024-02-01 11:07:14 +0300

    kurulum

---
+++ 4-kurs/screens/ManageCourse.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function ManageCourse() {
+    return (
+        <View>
+            <Text>ManageCourse</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({})
